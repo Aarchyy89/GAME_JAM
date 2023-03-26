875224b69c1b29a6d7c09fef1fac501fcdfd9659
Aarchyy89 <118544815+Aarchyy89@users.noreply.github.com>
2023-03-26 18:20:09 +0200

    Random Door
    yepa

---
+++ Project/Assets/Scripts/Spawn_p/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] SpawnLocations;
+    public GameObject door;
+
+    public Vector3 respawnLocation;
+
+    private void Awake()
+    {
+        SpawnLocations = GameObject.FindGameObjectsWithTag("SpawnPoint");
+    }
+
+    private void Start()
+    {
+        door = (GameObject)Resources.Load("salida", typeof(GameObject));
+
+        respawnLocation = door.transform.position;
+
+        SpawnDoor();
+    }
+
+    private void SpawnDoor()
+    {
+        int spawn = Random.Range(0, SpawnLocations.Length);
+        GameObject.Instantiate(door, SpawnLocations[spawn].transform.position, Quaternion.identity);
+    }
+}
