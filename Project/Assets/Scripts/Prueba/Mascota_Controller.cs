681d440b27fd182206159024901b7151950a0dd4
guiler126 <128089192+guiler126@users.noreply.github.com>
2023-03-27 08:39:47 +0200

    modo locura done

--- Project/Assets/Scripts/Prueba/Mascota_Controller.cs
+++ Project/Assets/Scripts/Prueba/Mascota_Controller.cs
@@ -1,22 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Mascota_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Parameters")]
+    public float player_locura = 0f;
+    [SerializeField] private float max_locura = 100.0f;
+    [SerializeField] private bool we_Are_flashing = false;
+    
+       
+
+    [Header("Stamina_Parameters")]
+    [Range(0f, 50f)][SerializeField] private float Stamina_Drain = 20f;
+    [Range(0f, 50f)][SerializeField] private float Stamina_Regen = 0.5f;
+
+    [Header("Stamina_UI_Elements")]
+    [SerializeField] private Image stamina_Progress = null;
+    [SerializeField] private CanvasGroup slider_grp = null;
+
 
     private void Start()
     {
         Locura_Controller.instance.GetComponent<Locura_Controller>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
+    {
+        if (we_Are_flashing == false)
+        {
+            player_locura -= Stamina_Regen + Time.deltaTime;
+            UpdateStamina(1);
+
+            if (player_locura <= 0)
+            {
+                player_locura = 0;
+                slider_grp.alpha = 0;
+            }
+
+            if(player_locura >= 1)
+            {
+                slider_grp.alpha = 1;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Enemigo"))
         {
-            other.GetComponent<enemigoBasico>().StopEnemy();
-            Locura_Controller.instance.Flashing();
+            other.GetComponent<enemigoBasico>().StopEnemy(); 
+            Flashing();
 
         }
     }
@@ -25,7 +60,34 @@ public class Mascota_Controller : MonoBehaviour
     {
         if (other.CompareTag("Enemigo"))
         {
+            we_Are_flashing = false;
             other.GetComponent<enemigoBasico>().GoToTarget();
+            
+        }
+    }
+    
+    public void Flashing()
+    {
+        we_Are_flashing = true;
+        player_locura += Stamina_Drain * Time.deltaTime;
+        UpdateStamina(1);
+
+        if (player_locura == 100)
+        {
+            Level_Manager.instance.panel_locura.gameObject.SetActive(true); 
+        }
+    }
+    public void UpdateStamina(int value)
+    {
+        stamina_Progress.fillAmount = player_locura / max_locura;
+
+        if (value == 0)
+        {
+            slider_grp.alpha = 0;
+        }
+        else
+        {
+            slider_grp.alpha = 1;
         }
     }
 }
